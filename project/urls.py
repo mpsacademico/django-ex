255b99723a134af61bae9d207dab0520af57fd0d
Your Name <you@example.com>
2018-03-13 13:29:27 -0300

    Adição de funções para inserção de dados para teste

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from welcome.views import index, health, mongo, show
 
 urlpatterns = [
     # Examples:
@@ -11,6 +11,8 @@ urlpatterns = [
 
     url(r'^$', index),
     url(r'^health$', health),
+    url(r'^show/$', show),
+    url(r'^mongo/$', mongo),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
--- welcome/views.py
+++ welcome/views.py
@@ -6,17 +6,34 @@ from django.http import HttpResponse
 from . import database
 from .models import PageView
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client['general']
+sensor = db['sensor']
+
 # Create your views here.
 
 def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
+   
 
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
         'database': database.info(),
-        'count': PageView.objects.count()
+        'count': 1
     })
 
 def health(request):
-    return HttpResponse(PageView.objects.count())
+    return HttpResponse(1)
+
+def show(request):
+	text = ""
+	page = sensor.find({})
+	for p in page:
+		text = text + "<br>" + str(p)
+	return HttpResponse(text)
+
+def mongo(request):
+    data = {"serialnumber": request.GET.get('serialnumber'), "key": request.GET.get('key'), "data": request.GET.get('data')}
+    teste = sensor.insert_one(data).inserted_id
+    return HttpResponse(teste)
